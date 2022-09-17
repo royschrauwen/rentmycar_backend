50ae47efa01cced83a9cc4a206d434d00b6fe7c1
Roy Schrauwen <roy@royschrauwen.nl>
2022-09-17 08:02:23 +0200

    Spring Boot Setup

---
+++ src/test/java/com/example/sprintboottutorial/SprintBootTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sprintboottutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprintBootTutorialApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
